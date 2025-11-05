484d486b3cc54aabdff671da37d9568307ccabc3
mezaaf <mezafuwandhana@gmail.com>
2025-11-05 16:16:32 +0700

    feat: pre-commit husky

---
+++ src/features/home/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
