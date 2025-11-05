a4d13a3aa996df42106e6fc35215220bcfa01f31
mezaaf <mezafuwandhana@gmail.com>
2025-11-05 16:21:28 +0700

    feat: prisma setup

---
+++ prisma/migrations/20251105092015_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Court" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Court_pkey" PRIMARY KEY ("id")
+);
