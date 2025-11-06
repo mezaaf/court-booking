3f1e6a3db7040d35fba9fa6e2f6bb1b6b1a74487
mezaaf <mezafuwandhana@gmail.com>
2025-11-06 20:52:23 +0700

    fix: redirecting after logout for all session

--- src/components/common/LogoutEverywhereButton.tsx
+++ src/components/common/LogoutEverywhereButton.tsx
@@ -2,12 +2,21 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { authClient } from "@/server/auth/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const LogoutEverywhereButton = () => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const handleLogoutEverywhere = async () => {
     setIsLoading(true);
-    await authClient.revokeSessions();
+    const { error } = await authClient.revokeSessions();
+    if (error) {
+      toast.error("Gagal logout dari semua perangkat.");
+    } else {
+      router.replace("/");
+    }
+    setIsLoading(false);
   };
   return (
     <Button
