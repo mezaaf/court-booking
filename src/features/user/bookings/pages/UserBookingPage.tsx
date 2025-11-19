acf5f2ea50a9bda24fcd4638e7f7a0ab1a50dbe8
mezaaf <mezafuwandhana@gmail.com>
2025-11-19 07:59:57 +0700

    fix(user booking): add fallback when booking data not exist

--- src/features/user/bookings/pages/UserBookingPage.tsx
+++ src/features/user/bookings/pages/UserBookingPage.tsx
@@ -7,18 +7,24 @@ import { useUserBookingPage } from "../hooks/useUserBookingPage";
 const UserBookingPage = () => {
   const { bookings, total, isLoading } = useUserBookingPage();
   return (
-    <div className="flex flex-col gap-4 sm:gap-6 lg:gap-8 w-full max-w-7xl px-4  sm:px-6 lg:px-8 mx-auto py-16 sm:py-20 lg:py-24">
+    <div className="flex flex-col gap-4 sm:gap-6 lg:gap-8 w-full max-w-7xl px-4  sm:px-6 lg:px-8 mx-auto py-16 sm:py-20 lg:py-24 min-h-screen">
       <h1 className="text-2xl font-bold">Riwayat Booking</h1>
       <div className="w-full grid grid-cols-3 gap-4">
-        {bookings.map((booking) => (
-          <DetailBookingCard
-            key={booking.id}
-            booking={booking}
-            isLoading={isLoading}
-          />
-        ))}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : bookings.length === 0 ? (
+          <p>Belum ada riwayat booking.</p>
+        ) : (
+          bookings.map((booking) => (
+            <DetailBookingCard
+              key={booking.id}
+              booking={booking}
+              isLoading={isLoading}
+            />
+          ))
+        )}
       </div>
-      <PaginationData total={total} />
+      <PaginationData total={total} isLimit={false} />
     </div>
   );
 };
